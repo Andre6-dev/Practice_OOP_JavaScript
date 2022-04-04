73c277bb1c4cbaf35ba86ce5750e2dc49e869936
Andre <andre.gallegos@tecsup.edu.pe>
2022-04-03 22:47:54 -0500

    Parsing method references around.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,9 @@ class Tooltip {
 class ProjectItem {
 
     // create the id
-    constructor(id) {
+    constructor(id, updateProjectsListsFunction) {
         this.id = id;
+        this.updateProjectListsHandler = updateProjectsListsFunction
         this.connectMoreInfoButton()
         this.connectSwitchButton();
     }
@@ -19,7 +20,7 @@ class ProjectItem {
         const projectItemElement = document.getElementById(this.id)
         // selecting the button individually
         const switchBtn = projectItemElement.querySelector('button:last-of-type');
-        switchBtn.addEventListener('click', )
+        switchBtn.addEventListener('click', this.updateProjectListsHandler)
     }
 
 }
@@ -29,25 +30,30 @@ class ProjectList {
 
     // parse the project type
     constructor(type) {
+        this.type = type;
         // select based on id of finish or active and the list item
         const prjItems = document.querySelectorAll(`#${type}-projects li`);
         //get the id with the loop
         for (const prjItem of prjItems) {
-            this.projects.push(new ProjectItem(prjItem.id));
+            this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
         }
         console.log(this.projects);
     }
 
-    addProject() {
+    setSwitchHandlerFunction(switchHandlerFunction) {
+        this.switchHandler = switchHandlerFunction;
+    }
 
+    addProject() {
+        console.log(this);
     }
 
     switchProject(projectId) {
         // const projectIndex = this.projects.findIndex(p => p.id === projectId);
         // // delete the project based on its index.
         // this.projects.splice(projectIndex, 1)
-        this.addProject();
-        this.projects = this.projects.filter(p => p.id !== projectId)
+        this.switchHandler(this.projects.find(p => p.id === projectId));
+        this.projects = this.projects.filter(p => p.id !== projectId);
     }
 }
 
@@ -56,6 +62,9 @@ class App {
     static init() {
         const activeProjectsList = new ProjectList('active');
         const finishedProjectsList = new ProjectList('finished');
+        //setting active to finished
+        activeProjectsList.setSwitchHandlerFunction(finishedProjectsList.addProject.bind(finishedProjectsList));
+        finishedProjectsList.setSwitchHandlerFunction(activeProjectsList.addProject.bind(finishedProjectsList));
     }
 
 }
